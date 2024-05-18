794c846a7d65a27c3d6abecb533fd1e6a1a844d8
vishal tripathi <vishal.tripathi@telekom-digital.com>
2024-05-18 16:33:03 +0530

    Added No Cors

---
+++ src/main/java/com/hackfest/RecommendationSystem/config/NoSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.hackfest.RecommendationSystem.config;//package hal.telekom.product.catalog.admin.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Profile("default | local")
+public class NoSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable).
+                cors(AbstractHttpConfigurer::disable).
+                authorizeHttpRequests(request -> request.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
